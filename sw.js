2cdccfbf35d0d8fec56244281f1c78cdab9193e5
sangatbutuh604-dot <sangatbutuh604@gmail.com>
2026-09-04 18:48:01 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker sederhana untuk aplikasi Absen
 // Menyimpan file inti agar aplikasi tetap bisa dibuka walau sinyal internet lemah/hilang
-const CACHE_NAME = 'absen-cache-v1';
+const CACHE_NAME = 'absen-cache-v2';
 const CORE_FILES = [
   './index.html',
   './manifest.json',
